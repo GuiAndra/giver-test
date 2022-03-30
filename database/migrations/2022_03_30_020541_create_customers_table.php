4b3eeb63949680f5e45c4c2a514d0c581e4e0b5b
Guilherme <gui.andra142@gmail.com>
2022-03-30 13:51:10 -0300

    create customer model and migration

---
+++ database/migrations/2022_03_30_020541_create_customers_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name', 255);
+            $table->string('last_name', 255)->nullable();
+            $table->string('email', 255)->nullable();
+            $table->string('gender', 50)->nullable();
+            $table->string('ip_address', 15)->nullable();
+            $table->string('company', 50)->nullable();
+            $table->string('city', 50)->nullable();
+            $table->string('title', 100)->nullable();
+            $table->string('website', 1500)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+};
